63cca7f915245d34d4f6ae17e94d48c39fcaf79f
Christina Roise <christina@netron.no>
2020-11-14 21:01:02 +0100

    imported useState, and created a function for button to toggle icon when on click

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './searchBar.css';
 import { ReactComponent as MagnifyingGlass } from '../assets/magnifying-glass.svg';
+import { ReactComponent as Exit } from '../assets/cross.svg';
 
-const SearchBar = (props) => {
+const SearchBar = (props) =>{
+    let [icon, setIcon] = useState(<MagnifyingGlass/>)
+    let [buttonClicked, setButtonClicked] = useState(false)
+
+    const onClickSearch = () => {
+        setButtonClicked(!buttonClicked)
+        if(!buttonClicked){
+            setIcon(<Exit/>)
+        }else{
+            setIcon(<MagnifyingGlass/>)
+        }
+    }
+
+    function test (){
+        console.log('test')
+    }
+
     return (
         <div className="search-box">
             <input className="search-input-value" type="text" autoComplete="off" value={props.inputValue} onChange={props.onChange} placeholder="Search"/>
-    <button className="search-button" type="submit" onClick={props.onSearchClick}><MagnifyingGlass/></button>
+            <button className="search-button" type="submit" onClick={() => {onClickSearch()}}>{icon}</button>
         </div>
     );
 };
 
-export default SearchBar;
+export default SearchBar 

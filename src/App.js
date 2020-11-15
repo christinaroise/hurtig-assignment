2ec0fed94b5de148ea1f3f0f039e8f20c4916ceb
Christina Roise <christina@netron.no>
2020-11-16 00:22:10 +0100

    added two comments in app.js  about console messages on first load

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { useState } from 'react';
 import SearchBar from './components/SearchBar.js';
 import ListOfShips from './components/ListOfShips.js';
 
+// ERROR message in console: Manifest: Line: 10, column: 3, Unexpected token. manifest.json:10 
+// I have briefly looked into this, but this is outside of my knowledge
+
+// WARNING messages also warns about missing dependencies: serch and shipsURL. 
+// I suspect the solution is to restructure the code (just adding the two in useEffect creates new error messages)
+
 function App() {
   const [searchValue, setSearchValue] = useState("")
   
